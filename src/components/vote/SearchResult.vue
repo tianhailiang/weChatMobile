1517deab0f22f42891f939d30d26a8c3f4c387f3
thl <tianhailiang50@163.com>
2017-05-27 19:24:40 +0800

    增加搜索界面

---
+++ src/components/vote/SearchResult.vue
@@ -0,0 +1,66 @@
+
+<style scoped>
+
+
+</style>
+
+<template>
+	<div>
+		
+		搜索界面
+
+	</div>
+
+</template>
+
+<script>
+
+	import axios from 'axios';
+
+	export default {
+  name: 'VoteSchool',
+  data () {
+    return {
+     
+    }
+  },
+  components: {
+    
+  },
+
+  methods:{
+
+  },
+  
+  mounted:function(){
+
+      axios.get("http://localhost:7777/static/getmock/searchResult.json",{
+
+           })
+          .then(function (response) {
+
+            var result =response.data;
+
+              if(result.code==0){
+
+              
+
+                
+              }else{
+
+                console.log(result.msg)
+              } 
+             
+          }.bind(this))
+          .catch(function (error) {
+              console.log(error);
+          });
+
+
+  }
+
+ } 
+
+
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import VoteSchool from '@/components/vote/VoteSchool' //投票学校模块
 import VoteClass from '@/components/vote/VoteClass' //投票班级模块
 import Contestant from '@/components/vote/Contestant' //投票选手列表模块
 import TalentSpace from '@/components/vote/TalentSpace' //我的才艺空间模块
+import SearchResult from '@/components/vote/SearchResult' //搜索结果模块
 
 
 Vue.use(Router)
@@ -38,6 +39,14 @@ export default new Router({
       component: Contestant,
       meta:{title:"投票"}
     },
+    
+    {
+      path: '/searchResult',
+      name: 'searchResult',
+      component: SearchResult,
+      meta:{title:"投票"}
+    },
+
     {
       path: '/talentSpace/:id',
       name: 'talentSpace',
