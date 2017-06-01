3928c084b782fdf225a9fae59ed2aab4dca0a416
Hemingway <345612905@qq.com>
2017-06-01 16:02:53 +0800

    打开了git忽略里的dist压缩文件

---
+++ src/components/personalCenter/personalCenter.vue
@@ -0,0 +1,20 @@
+<!--&lt;!&ndash; Add "scoped" attribute to limit CSS to this component only &ndash;&gt;-->
+<!--<style scoped>-->
+
+
+<!--</style>-->
+
+
+<!--<template>-->
+  <!--<div>-->
+    <!--<div class="box">-->
+
+    <!--</div>-->
+  <!--</div>-->
+
+
+<!--</template>-->
+
+<!--<script>-->
+
+<!--</script>-->
